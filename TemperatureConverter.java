715cab3b80d0222946ba39d1a30456014f8ac4df
Reppiks <30858986+Reppiks@users.noreply.github.com>
2019-03-27 12:25:08 -0500

    Add files via upload

---
+++ TemperatureConverter.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class TemperatureConverter {
+
+  
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter 1 to convert a Fehrenheit temperature. Enter 2 to convert a Celsius temperature: ");
+        
+        double t = input.nextInt();
+        double n = 0;
+        double start = 0.0;
+        double converted = 0;
+        
+        if (t == 1){
+            System.out.print("please enter a Fehrenheit temperature to convert: ");
+            start = input.nextDouble();
+            converted = (start - 32) / 1.8d;
+            System.out.printf("%.1f degrees Fehrenheit equals %.1f degrees Celsuius.%n", start, converted);
+        }
+        
+        else if (t == 2) {
+            System.out.print("please enter a Celsius temperature to convert: ");
+            start = input.nextDouble();
+            converted = (start * 1.8d) + 32;
+            System.out.printf("%.1f degrees Celsius equals %.1f degrees Fehrenheit.%n", start, converted);
+        }
+        else {
+            System.out.println("Error.  Please try another number.");
+        }
+    }
+    
+}
