87dd24a838d5b3dfb0cd3da818ac7914d44933e3
Reppiks <30858986+Reppiks@users.noreply.github.com>
2019-03-29 15:02:57 -0500

    Update TemperatureConverter.java

--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -1,35 +1,36 @@
 import java.util.Scanner;
 
-class TemperatureConverter {
+public class TemperatureConverterSwitch {
 
-  
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
         
+       
         System.out.print("Enter 1 to convert a Fehrenheit temperature. Enter 2 to convert a Celsius temperature: ");
         
-        double t = input.nextInt();
+        int selection = input.nextInt();
         double start = 0.0;
         double converted = 0;
         
-        if (t == 1){
-            System.out.print("please enter a Fehrenheit temperature to convert: ");
-            start = input.nextDouble();
-            converted = (start - 32) / 1.8d;
-            System.out.printf("%.1f degrees Fehrenheit equals %.1f degrees Celsuius.%n", start, converted);
-        }
+        switch (selection) {
+            case 1:
+                System.out.print("please enter a Fehrenheit temperature to convert: ");
+                start = input.nextDouble();
+                converted = (start - 32) / 1.8d;
+                System.out.printf("%.1f degrees Fehrenheit equals %.1f degrees Celsuius.%n", start, converted);
+                break;
         
-        else if (t == 2) {
-            System.out.print("please enter a Celsius temperature to convert: ");
-            start = input.nextDouble();
-            converted = (start * 1.8d) + 32;
-            System.out.printf("%.1f degrees Celsius equals %.1f degrees Fehrenheit.%n", start, converted);
+            case 2:
+                System.out.print("please enter a Celsius temperature to convert: ");
+                start = input.nextDouble();
+                converted = (start * 1.8d) + 32;
+                System.out.printf("%.1f degrees Celsius equals %.1f degrees Fehrenheit.%n", start, converted);
+                break;
+            
+            default:
+                System.out.println("Error.  Please try another number.");
         }
-        else {
-            System.out.println("Error.  Please try another number.");
-        }
-    }
-    
+              
+    }    
 }
-
